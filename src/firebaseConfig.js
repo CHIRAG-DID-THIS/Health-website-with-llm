251b91145c462b32a6b1183429de685f85594ab6
CHIRAG-DID-THIS <131998970+CHIRAG-DID-THIS@users.noreply.github.com>
2024-12-17 23:52:31 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -8,14 +8,6 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCr4ykd8ZfYmFtJtFXYaxQwTRos1RHUeAk",
-  authDomain: "therapy-fc975.firebaseapp.com",
-  databaseURL: "https://therapy-fc975-default-rtdb.firebaseio.com",
-  projectId: "therapy-fc975",
-  storageBucket: "therapy-fc975.appspot.com",
-  messagingSenderId: "761053931587",
-  appId: "1:761053931587:web:e0bb1f63d0cef566290cf6",
-  measurementId: "G-QJDHDB457H"
 };
 
 // Initialize Firebase
